bcec4a93e352778812699e2c301ba3b5b31e0080
Deen Dayal Sharma <dsharma@ecisolutions.com>
2020-07-27 23:49:36 -0500

    Add project files.

---
+++ WebAPIWithDependencyInjection/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebAPIWithDependencyInjection.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
